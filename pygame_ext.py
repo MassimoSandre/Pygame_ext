512d6f27c56dc3e46c34efb4411dd72f759ccc34
Massimo Sandretti <massimo.sandretti@gmail.com>
2022-02-02 03:01:56 +0100

    implemented basic stuff for looping pygame

--- pygame_ext.py
+++ pygame_ext.py
@@ -1,5 +1,17 @@
 import pygame
+from pygame.locals import *
 
 class Pygame_ext:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,*, size=(620,540), bgcolor=(0,0,0), tick_time=60, resizable=False) -> None:
+        self.size = self.width, self.height = size
+        self.clock = pygame.time.Clock()
+        self.tick_time = tick_time
+        self.bgcolor = bgcolor
+
+        if resizable:
+            self.screen = pygame.display.set_mode(size, RESIZABLE)
+
+    def loop(self):
+        pygame.display.update()
+        self.clock.tick(self.tick_time)
+        self.screen.fill(self.bgcolor)
